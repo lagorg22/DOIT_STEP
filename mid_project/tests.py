22a0afef1f6d76b4160f05d75775eb961be4ba44
lagorg22 <lagorg22@freeuni.edu.ge>
2024-11-26 02:05:21 +0400

    final commit

--- mid_project/tests.py
+++ mid_project/tests.py
@@ -6,22 +6,22 @@ m = Menu()
 f = Faker()
 grades = ['A', 'B', 'C', 'D', 'E', 'F']
 
-print('<========================Testing addition================================>')
-for i in range(800):
-    name = f.name() + ' ' + f.last_name()
-    roll_number = i
-    grade = random.choice(grades)
-    m.add_student(name, roll_number, grade)
-
-print('<========================Testing grade change================================>')
-for _ in range(400):
-    roll_number = random.randint(0, 400)
-    grade = random.choice(grades)
-    m.change_grade(roll_number, grade)
+# print('<========================Testing addition================================>')
+# for i in range(1000):
+#     name = f.name() + ' ' + f.last_name()
+#     roll_number = i
+#     grade = random.choice(grades)
+#     m.add_student(name, roll_number, grade)
+#
+# print('<========================Testing grade change================================>')
+# for _ in range(400):
+#     roll_number = random.randint(0, 400)
+#     grade = random.choice(grades)
+#     m.change_grade(roll_number, grade)
 
 print('<========================Testing student removal================================>')
-for _ in range(100):
-    roll_number = random.randint(0, 400)
+for i in range(60, 800):
+    roll_number = i
     m.remove_student(roll_number)
 
 
